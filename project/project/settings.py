8407e20503454fa033cadf7c8a9dce8d292933c5
Bill Doody <wdoody@yahoo.com>
2018-05-24 19:38:03 -0600

    changeup

--- project/project/settings.py
+++ project/project/settings.py
@@ -15,9 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR, 'build/static'),
 ]
